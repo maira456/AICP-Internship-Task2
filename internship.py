a35f862c7133ad1961a8ec565cdfef7940d6176c
maira456 <149946281+maira456@users.noreply.github.com>
2023-11-12 18:55:33 +0500

    internship.py

--- internship.py
+++ internship.py
@@ -1,42 +1,46 @@
 # Start of the day
 journeys = {
-    "09:00": {"up": 0, "down": 0, "revenue": 0},
-    "12:00": {"up": 0, "down": 0, "revenue": 0},
-    "14:00": {"up": 0, "down": 0, "revenue": 0},
-    "15:00": {"up": 0, "down": 0, "revenue": 0}
+       "09:00": {"up": 0, "down": 0, "revenue": 0},
+       "12:00": {"up": 0, "down": 0, "revenue": 0},
+       "14:00": {"up": 0, "down": 0, "revenue": 0},
+       "15:00": {"up": 0, "down": 0, "revenue": 0}
 }
 
 # Purchase tickets
 def purchase_tickets():
-    journey_time = input("Enter the journey time (in HH:MM format): ")
-    num_passengers = int(input("Enter the number of passengers: "))
+  print(""" Welcome to the ticket Purchase Station.
+             Our Journey times:""")
+  for journey_time in journeys.keys():
+      print(journey_time)
+  journey_time = input("Enter the journey time (in HH:MM format): ")
+  num_passengers = int(input("Enter the number of passengers: "))
 
     # Check if the journey time is valid
-    if journey_time not in journeys.keys():
+  if journey_time not in journeys.keys():
         print("Invalid time.")
         return
 
-    journey = journeys[journey_time]
+  journey = journeys[journey_time]
 
     # Check if there are enough tickets available
-    if num_passengers * 2 > 80 * 6:
+  if num_passengers * 2 > 80 * 6:
         print("Not enough seats available.")
         return
 
     # Calculate the total price including group discount
-    price_per_ticket = 25
-    total_price = num_passengers * price_per_ticket
+  price_per_ticket = 25
+  total_price = num_passengers * price_per_ticket
 
-    if num_passengers >= 10 and num_passengers <= 80:
+  if num_passengers >= 10 and num_passengers <= 80:
         free_tickets = num_passengers // 10
         total_price -= free_tickets * price_per_ticket
 
     # Update the screen display and the data for the totals
-    journey["up"] += num_passengers
-    journey["revenue"] += total_price
-
-    print("Tickets purchased successfully.")
-    print(f"Total amount paid: ${total_price}")
+  journey["up"] += num_passengers
+  journey["revenue"] += total_price
+  
+  print("Tickets purchased successfully.")
+  print(f"Total amount paid: ${total_price}")
 
 # End of the day report
 def end_of_day():
@@ -67,19 +71,20 @@ def end_of_day():
 # Main execution
 print("Start of the day.")
 
-print("Journey times:")
-for journey_time in journeys.keys():
-    print(journey_time)
+
 
 while True:
-    ch = input("Enter '1' to purchase tickets, '2' to see the end day, or 'c' to close: ")
+    ch = input("""1.Purchase Ticket 
+    2.See the end day
+    3.Quit(q)
+    please enter code for your choice.   """)
 
     if ch == '1':
         purchase_tickets()
     elif ch == '2':
         end_of_day()
         break
-    elif ch.lower() == 'c':
+    elif ch.lower() == 'q':
         break
     else:
         print("Invalid choice.")
